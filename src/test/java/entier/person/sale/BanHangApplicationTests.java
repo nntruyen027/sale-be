04e0aa5b4071fd1d03a9f320d1ab41f7636e68d0
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2026-01-14 15:50:12 +0700

    khởi tạo test

--- src/test/java/entier/person/sale/BanHangApplicationTests.java
+++ src/test/java/entier/person/sale/BanHangApplicationTests.java
@@ -2,12 +2,14 @@ package entier.person.sale;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BanHangApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
