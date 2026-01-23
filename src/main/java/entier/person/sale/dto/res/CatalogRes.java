0f89892824cbebe069153697c6bd0f438e2206fe
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2026-01-23 16:18:17 +0700

    bổ sung ngày tạo catalog

--- src/main/java/entier/person/sale/dto/res/CatalogRes.java
+++ src/main/java/entier/person/sale/dto/res/CatalogRes.java
@@ -2,9 +2,12 @@ package entier.person.sale.dto.res;
 
 import lombok.Data;
 
+import java.sql.Timestamp;
+
 @Data
 public class CatalogRes {
     Long id;
     String tieuDe;
     String url;
+    Timestamp ngayTao;
 }
